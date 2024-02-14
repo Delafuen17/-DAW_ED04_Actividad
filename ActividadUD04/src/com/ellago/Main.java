e5ab21afb3da35dc935b0722b834d85a25556035
Papii19 <guillencristian97@gmail.com>
2024-02-14 10:11:02 +0100

    Paso 2:He introducido el método operativa_cuenta que engloba las setencias de la clase main.
    Paso 3:He encapsulado los 4 atributos de la clase Ccuenta

--- ActividadUD04/src/com/ellago/Main.java
+++ ActividadUD04/src/com/ellago/Main.java
@@ -5,27 +5,32 @@
 
 package com.ellago;
 
-
 public class Main {
 
     public static void main(String[] args) {
         CCuenta Cuenta1;
         double saldoActual;
 
-        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        Cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         saldoActual = Cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es " + saldoActual);
+
+        operativa_cuenta(Cuenta1);
+    }
+
+   
+    private static void operativa_cuenta(CCuenta cuenta) {
+        try {
+            cuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.println("Fallo al retirar: " + e.getMessage());
+        }
 
         try {
-            Cuenta1.retirar(2300);
-        } catch (Exception e) {
-            System.out.print("Fallo al retirar");
-        }
-        try {
             System.out.println("Ingreso en cuenta");
-            Cuenta1.ingresar(695);
+            cuenta.ingresar(695);
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.println("Fallo al ingresar: " + e.getMessage());
         }
     }
 }
